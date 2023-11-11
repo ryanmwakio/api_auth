ea8a082c22a663b7a07e54f3d857281b299b9fbd
Ryan Mwakio <ryanmwakio6@gmail.com>
2023-11-11 19:49:40 +0300

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->count(5)->has(Task::factory()->count(5))->create();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+require_once __DIR__ . "/api/v1.php";
+require_once __DIR__ . "/api/v2.php";
+
+Route::prefix('auth')->group(function () {
+    Route::post('login', LoginController::class)->name('login');
+    Route::post('logout', LogoutController::class)->name('logout')->middleware('auth:sanctum');
+    Route::post('register', RegisterController::class)->name('register');
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
