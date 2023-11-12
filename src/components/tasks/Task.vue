98166245752ea932dc08b78fa7c6240976e7dbbb
Ryan Mwakio <ryanmwakio6@gmail.com>
2023-11-12 14:35:04 +0300

    initial client commit

---
+++ src/components/tasks/Task.vue
@@ -0,0 +1,46 @@
+<script setup>
+import IconPencil from "@/components/icons/IconPencil.vue";
+import IconTrash from "@/components/icons/IconTrash.vue";
+import { computed } from "vue";
+const props = defineProps({
+  task: Object,
+});
+
+const completedClass = computed(() =>
+  props.task.is_completed ? "completed" : ""
+);
+</script>
+
+<template>
+  <li class="list-group-item py-3">
+    <div class="d-flex justify-content-start align-items-center">
+      <input
+        class="form-check-input mt-0"
+        :class="completedClass"
+        type="checkbox"
+        :checked="task.is_completed"
+      />
+      <div
+        class="ms-2 flex-grow-1"
+        :class="completedClass"
+        title="Double click the text to edit or remove"
+      >
+        <!-- <div class="relative">
+                                            <input class="editable-task" type="text" />
+                                        </div> -->
+        <span>{{ task.name }}</span>
+      </div>
+      <div class="task-date">24 Feb 12:00</div>
+    </div>
+    <div class="task-actions">
+      <button class="btn btn-sm btn-circle btn-outline-secondary me-1">
+        <IconPencil />
+      </button>
+      <button class="btn btn-sm btn-circle btn-outline-danger">
+        <IconTrash />
+      </button>
+    </div>
+  </li>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/TasksPage.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { allTasks } from "@/http/task-api";
+import Task from "@/components/tasks/Task.vue";
+
+const tasks = ref([]);
+
+onMounted(async () => {
+  const { data } = await allTasks();
+  tasks.value = data.data;
+});
+</script>
+
+<template>
+  <main style="min-height: 50vh; margin-top: 2rem">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <!-- Add new Task -->
+          <div class="relative">
+            <input
+              type="text"
+              class="form-control form-control-lg padding-right-lg"
+              placeholder="+ Add new task. Press enter to save."
+            />
+          </div>
+          <!-- List of tasks -->
+          <div class="card mt-2">
+            <ul class="list-group list-group-flush">
+              <Task v-for="task in tasks" :key="task.id" :task="task" />
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
