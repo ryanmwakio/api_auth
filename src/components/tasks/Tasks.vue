0b187f21e3a341c77208559ce05bf2308cc5dc11
Ryan Mwakio <ryanmwakio6@gmail.com>
2023-11-12 23:54:51 +0300

    add task and unshift current accessible tasks

---
+++ src/components/tasks/Tasks.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineComponent, defineProps } from "vue";
+import Task from "@/components/tasks/Task.vue";
+
+defineProps({
+  tasks: Array,
+  show: {
+    type: Boolean,
+    default: true,
+  },
+});
+</script>
+
+<template>
+  <div class="card mt-2" v-if="tasks.length && show">
+    <ul class="list-group list-group-flush">
+      <Task v-for="task in tasks" :key="task.id" :task="task" />
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -1,9 +1,34 @@
 <script setup>
-import { onMounted, ref } from "vue";
-import { allTasks } from "@/http/task-api";
-import Task from "@/components/tasks/Task.vue";
+import { computed, onMounted, ref } from "vue";
+import { allTasks, createTask } from "@/http/task-api";
+import Tasks from "@/components/tasks/Tasks.vue";
+import NewTask from "../components/tasks/NewTask.vue";
 
 const tasks = ref([]);
+
+const uncompletedTasks = computed(() =>
+  tasks.value.filter((task) => !task.is_completed)
+);
+
+const completedTasks = computed(() =>
+  tasks.value.filter((task) => task.is_completed)
+);
+
+const showToggleCompletedBtn = computed(
+  () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
+);
+
+const completedTasksIsVisible = computed(
+  () => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0
+);
+
+const showCompletedTasks = ref(false);
+
+const handleAddedTask = async (newTask) => {
+  const { data: createdTask } = await createTask(newTask);
+
+  tasks.value.unshift(createdTask.data);
+};
 
 onMounted(async () => {
   const { data } = await allTasks();
@@ -17,19 +42,27 @@ onMounted(async () => {
       <div class="row">
         <div class="col-md-8 offset-md-2">
           <!-- Add new Task -->
-          <div class="relative">
-            <input
-              type="text"
-              class="form-control form-control-lg padding-right-lg"
-              placeholder="+ Add new task. Press enter to save."
-            />
+          <NewTask @added="handleAddedTask" />
+
+          <!-- List of uncompleted tasks -->
+          <Tasks :tasks="uncompletedTasks" />
+
+          <!-- show toggle button -->
+          <div class="text-center my-3" v-show="showToggleCompletedBtn">
+            <button
+              class="btn btn-sm btn-secondary"
+              @click="(event) => (showCompletedTasks = !showCompletedTasks)"
+            >
+              <span v-if="!showCompletedTasks">Show completed</span>
+              <span v-else>Hide completed</span>
+            </button>
           </div>
-          <!-- List of tasks -->
-          <div class="card mt-2">
-            <ul class="list-group list-group-flush">
-              <Task v-for="task in tasks" :key="task.id" :task="task" />
-            </ul>
-          </div>
+
+          <!-- List of completed tasks -->
+          <Tasks
+            :tasks="completedTasks"
+            :show="completedTasksIsVisible && showCompletedTasks"
+          />
         </div>
       </div>
     </div>
